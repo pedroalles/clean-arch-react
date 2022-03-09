683334b33832c2ccad1995c72ac92815245dbf65
pedroalles <pedroalles@gmail.com>
2022-03-09 02:40:44 -0300

    feat: ensure Login will not show load spinner or error message on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,13 +1,14 @@
-import { FC } from 'react'
-import Spinner from '../spinner/spinner'
-
+import { FC, useContext } from 'react'
+import Spinner from '@/presentation/components/spinner/spinner'
 import Styles from './form-status-styles.scss'
+import Context from '@/presentation/contexts/form/form-context'
 
 const FormStatus: FC = () => {
+  const { isLoading, errorMessage } = useContext(Context)
   return (
-    <div className={Styles.errorWrap}>
-      <Spinner className={Styles.spinner} />
-      <span className={Styles.error}>Error</span>
+    <div data-testid="error-wrap" className={Styles.errorWrap}>
+      {isLoading && <Spinner className={Styles.spinner} />}
+      {errorMessage && <span className={Styles.error}>Error</span>}
   </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,10 +1,23 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Header, Input, FormStatus, Footer } from '@/presentation/components'
 import Styles from './login-styles.scss'
+import Context from '@/presentation/contexts/form/form-context'
 
-const Login: FC = () => (
-  <div className={Styles.login}>
+type StateProps = {
+  isLoading: Boolean
+  errorMessage: string
+}
+
+const Login: FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
+  return (
+    <div className={Styles.login}>
     <Header />
+
+    <Context.Provider value={state}>
     <form className={Styles.form}>
       <h2>Login</h2>
       <Input type="email" name="email" placeholder="Enter your e-mail"/>
@@ -13,8 +26,11 @@ const Login: FC = () => (
       <span className={Styles.link}>Sing Up</span>
       <FormStatus />
     </form>
+    </Context.Provider>
+
     <Footer />
   </div>
-)
+  )
+}
 
 export default Login
