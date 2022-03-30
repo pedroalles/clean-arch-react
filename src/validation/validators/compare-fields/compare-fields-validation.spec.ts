38b4c264c2276c6bfec5236d3073513509206816
pedroalles <pedroalles@gmail.com>
2022-03-30 00:35:17 -0300

    test: ensure CompareFieldsValidation returns null if validation succeeds

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -1,12 +1,20 @@
 import { InvalidFieldError } from '@/validation/errors'
+import faker from 'faker'
 import { CompareFieldsValidation } from './compare-fields-validation'
 
 const makeSut = (valueToCompare: string): CompareFieldsValidation => new CompareFieldsValidation('field', valueToCompare)
 
 describe('CompareFieldsValidation', () => {
   it('should return error if compare is invalid', () => {
-    const sut = makeSut('field')
-    const error = sut.validate('field2')
+    const sut = makeSut(faker.random.word())
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  it('should return null if compare is valid', () => {
+    const valueToCompare = faker.random.word()
+    const sut = makeSut(valueToCompare)
+    const error = sut.validate(valueToCompare)
+    expect(error).toBeNull()
   })
 })
